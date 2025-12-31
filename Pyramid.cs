feec3b6856403337f3f70f1c1a0870576cfc8ee8
Umair2840 <umairabbas2840@gmail.com>
2025-12-31 15:38:00 +0500

    Update and rename Program.cs to Pyramid.cs

--- Pyramid.cs
+++ Pyramid.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pyramid_Pattern
+namespace Number_Pyramid
 {
     internal class Program
     {
@@ -37,3 +37,4 @@ namespace Pyramid_Pattern
         }
     }
 }
+
