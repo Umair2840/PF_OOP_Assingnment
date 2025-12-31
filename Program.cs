6e54d037ca1b6149f5d5a5cff0a70844b3e72228
Umair2840 <umairabbas2840@gmail.com>
2025-12-31 14:55:07 +0500

    Added Pyramind pattern practise problem

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 8;
+            for (int row = 1; row < size; row++)
+            {
+                int incnum = 1,decnum = 1, num1 = 1,num2 = 2;
+                for (int col = size;  col > row; col--)
+                {
+                    Console.Write("     ");
+                }
+                while ( num1 <= row)
+                {
+                    if(incnum.ToString().Length == 1) Console.Write(incnum + "    ");
+                    else if(incnum.ToString().Length == 2){ Console.Write(incnum + "   "); }
+                    else {  Console.Write(incnum + "   ");}
+                        incnum *= 2; num1++;
+                }
+                decnum = incnum/2;
+                while (num2 <= row)
+                {
+                    decnum /= 2; num2++;
+                    if (num2 <= 4) Console.Write(decnum + "    ");
+                    else { Console.Write(decnum + "   "); }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
