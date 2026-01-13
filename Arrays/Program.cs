dad407a1332bb190a6e1f2365b26c9e8e2a5c0d1
Umair2840 <umairabbas2840@gmail.com>
2026-01-13 12:46:21 +0500

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _seond_max_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int secondmax = 0;
+
+            int[] array = { 8, 2, 3, 7 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    secondmax = max;
+                    max = array[i];
+                }
+                else if (array[i] > secondmax)
+                {
+                    secondmax = array[i];
+                }
+            }
+
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Second Max: " + secondmax);
+
+        }
+    }
+}
